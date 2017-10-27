c335b1f3e00edead7e20855f0467048d74b46744
Aman Dhally <amandhally@gmail.com>
2017-10-27 15:51:50 +0530

    Hash Demo

---
+++ Hash_in_PowerShell_Scripts.ps1
@@ -0,0 +1,3 @@
+﻿# In the PowerShell the HASH is a Comment.
+Write-Host "I will work......" -ForegroundColor Cyan -BackgroundColor Black
+#Write-Host "I will NOT work " -ForegroundColor Yellow -BackgroundColor White
